69a2f8d2a35301384eb9b0250ad4288f969e1d84
Matheus Ryan da Silva Nascimento <matheus.nascimento@edge.ufal.br>
2024-11-20 11:42:08 -0300

    [feat]: init conversion coin

--- Desafio 1.2/Exceptions/FutureAttendanceException.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Desafio_1._2.Exceptions
-{
-    internal class FutureAttendaceException : Exception
-    {
-        public FutureAttendaceException() : base("ERROR: O paciente já possui uma consulta a ser realizada") { }
-    }
-}
---
+++ Desafio 2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Exibe o menu de opções de entrada
+        Console.WriteLine("Conversor de Moeda");
+        Console.Write("Digite o valor a ser convertido: ");
+        double valorOrigem = Convert.ToDouble(Console.ReadLine());
+
+        // Solicita a taxa de conversão
+        Console.Write("Digite a taxa de conversão (1 unidade de origem para quantas unidades da moeda de destino): ");
+        double taxaConversao = Convert.ToDouble(Console.ReadLine());
+
+        // Calcula o valor convertido
+        double valorConvertido = valorOrigem;
+
+        // Exibe o valor convertido
+        Console.WriteLine($"Valor convertido: {valorConvertido}");
+    }
+}
